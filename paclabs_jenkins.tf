1e86b15eb99b04edfff6cbb6918608cd056f8d7d
Craig J Smith <nctiggy@gmail.com>
2017-05-26 14:35:05 -0700

    make it work

---
+++ paclabs_jenkins.tf
@@ -0,0 +1,65 @@
+# Configure the VMware vSphere Provider
+provider "vsphere" {
+  user           = "administrator@pac.lab"
+  password       = "Password#1"
+  vsphere_server = "10.4.44.11"
+
+  # if you have a self-signed cert
+  allow_unverified_ssl = true
+}
+
+# Create a folder
+resource "vsphere_folder" "frontend" {
+  datacenter = "Datacenter"
+  path = "support_services"
+}
+
+# Create a virtual machine within the folder
+resource "vsphere_virtual_machine" "jenkins" {
+  name   = "jenkins"
+  datacenter = "Datacenter"
+  domain = "jenkins.pac.lab"
+  dns_servers = ["10.4.44.10"]
+  cluster = "cluster"
+
+  folder = "${vsphere_folder.frontend.path}"
+  vcpu   = 2
+  memory = 2048
+
+  network_interface {
+    label = "VM Network"
+    ipv4_address = "10.4.44.12"
+    ipv4_prefix_length = "24"
+    ipv4_gateway = "10.4.44.1"
+  }
+
+  disk {
+    datastore = "datastore1"
+    template = "UbuntuTmpl"
+    type = "thin"
+  }
+
+  provisioner "file" {
+    source = "script.sh"
+    destination = "/tmp/script.sh"
+
+    connection {
+      type = "ssh"
+      user = "ubuntu"
+      password = "password"
+    }
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/script.sh",
+      "echo password | sudo -S /tmp/script.sh"
+    ]
+
+    connection {
+      type = "ssh"
+      user = "ubuntu"
+      password = "password"
+    }
+  }
+}
--- script.sh
+++ script.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-apt update --assume-yes
-apt upgrade --assume-yes
-apt install docker.io --assume-yes
-apt install ansible --assume-yes
+apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+apt-key fingerprint 0EBFCD88
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt update
+apt install docker-ce
 docker run --name PacLabsJenkins -p 80:80 -p 50000:50000 -v /var/jenkins_home jenkins
