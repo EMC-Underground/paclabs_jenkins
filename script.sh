9c740904177db99ea8faf267e5f44b32130a1317
Aaron Baldie <aaron.baldie@emc.com>
2017-05-28 15:25:48 -0700

    adding local logging, and new directory structure for container

--- script.sh
+++ script.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+LOGFILE=/tmp/script.log
+touch $LOGFILE
 
 apt install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
@@ -6,4 +8,5 @@ apt-key fingerprint 0EBFCD88
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt update -y
 apt install -y docker-ce docker-compose
-docker-compose -f /tmp/jenkins-compose.yml up -d
+sudo mkdir /var/lib/paclabs_ && sudo cp /tmp/jenkins-compose.yml /var/lib/paclabs_ >> $LOGFILE 2>&1
+sudo docker-compose -f /var/lib/paclabs_/jenkins-compose.yml up -d >> $LOGFILE 2>&1
