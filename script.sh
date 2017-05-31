4c4b25c7542150224fb64c08be6c4d8ba65ac12f
Craig J Smith <nctiggy@gmail.com>
2017-05-31 12:34:42 -0700

    update script to use current version of compose

--- script.sh
+++ script.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-LOGFILE=/tmp/script.log
-touch $LOGFILE
 
 apt install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 apt-key fingerprint 0EBFCD88
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt update -y
-apt install -y docker-ce docker-compose
-mkdir /var/lib/paclabs_ && sudo cp /tmp/jenkins-compose.yml /var/lib/paclabs_ >> $LOGFILE 2>&1
-docker-compose -f /var/lib/paclabs_/jenkins-compose.yml up -d >> $LOGFILE 2>&1
+apt install -y docker-ce
+curl -o /usr/local/bin/docker-compose -L "https://github.com/docker/compose/releases/download/1.11.2/docker-compose-$(uname -s)-$(uname -m)"
+chmod +x /usr/local/bin/docker-compose
+mkdir /var/lib/paclabs_ && sudo cp /tmp/jenkins-compose.yml /var/lib/paclabs_
+docker-compose -f /var/lib/paclabs_/jenkins-compose.yml up -d
